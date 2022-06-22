23d419430f88472c33d8e910b1d566c8356b3b84
Tyler Arbon <tyler@arbonwebsolutions.com>
2022-06-21 20:43:33 -0600

    Updated for submodule use

--- tests/BambooHRTest.php
+++ tests/BambooHRTest.php
@@ -8,5 +8,9 @@ use Zenapply\HRIS\BambooHR\BambooHR;
 
 class BambooHRTest extends TestCase
 {
-
+    public function testIncomplete() {
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+          );
+    }
 }
